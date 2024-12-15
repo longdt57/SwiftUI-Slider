fd0c0ae6fa35e0dd02e14a8c9efbd9f34cb3d08e
Long Do <Longdt57@gmail.com>
2024-12-15 17:45:10 +0900

    Initial Commit

---
+++ SwiftUI Slider/SwiftUI_Slider.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUI_Slider.h
+//  SwiftUI Slider
+//
+//  Created by Long Do on 15/12/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUI_Slider.
+FOUNDATION_EXPORT double SwiftUI_SliderVersionNumber;
+
+//! Project version string for SwiftUI_Slider.
+FOUNDATION_EXPORT const unsigned char SwiftUI_SliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUI_Slider/PublicHeader.h>
+
+
